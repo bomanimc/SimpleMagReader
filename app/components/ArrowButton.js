10550e2d6228f829766b7a215a01f215e0ba6442
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-10-15 23:28:39 -0700

    Brings in the classnames library for adding multiple classes to a JSX component, and positions arrows

--- app/components/ArrowButton.js
+++ app/components/ArrowButton.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
+import classNames from 'classnames/bind';
 import Assets from '../../public/assets/assets.map.json';
 import styles from '../styles/arrow.css';
+
+var cx = classNames.bind(styles);
 
 var ArrowButton = React.createClass( {
 	render: function() {
 		var arrow = (this.props.direction == 1) ? Assets.rightArrow : Assets.leftArrow;
+		var arrowClasses = cx({
+			arrowLeft: this.props.direction == -1,
+			arrowRight: this.props.direction == 1,
+			arrow: true
+		})
 		return (
-			<div className={styles.arrow, styles.arrowLeft}>
-				<img className={styles.arrowIcon} onClick={this.props.changePage} src={arrow} />
+			<div className={arrowClasses}>
+				<img className={cx('arrowIcon')} onClick={this.props.changePage} src={arrow} />
 			</div>
 		);
 	}
